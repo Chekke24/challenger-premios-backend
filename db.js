1597a25c569446f42d10ccac9f146b84d1c14bec
Francisco <fran_ortiz_18@hotmail.com>
2025-04-24 16:50:09 -0300

    arreglado el database

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
-require("dotenv").config();
-const { Pool } = require("pg");
+// require("dotenv").config();
+// const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
 
-pool.connect((err) => {
-  if (err) {
-    console.error("❌ Error al conectar a PostgreSQL:", err);
-  } else {
-    console.log("✅ Conectado a PostgreSQL");
-  }
-});
+// pool.connect((err) => {
+//   if (err) {
+//     console.error("❌ Error al conectar a PostgreSQL:", err);
+//   } else {
+//     console.log("✅ Conectado a PostgreSQL");
+//   }
+// });
 
-module.exports = pool;
+// module.exports = pool;
